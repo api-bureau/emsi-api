731691ff9c5cfea28e8f595f1cb26367b50fc0d0
Vaclav Elias <velias@burning-glass.com>
2021-08-07 13:32:34 +0100

    Added GetStatusAsync and GetMetaAsync

--- Emsi.Playground/Program.cs
+++ Emsi.Playground/Program.cs
@@ -1,7 +1,10 @@
+using Emsi.Playground.Dtos;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Emsi.Playground
@@ -9,6 +12,11 @@ namespace Emsi.Playground
     public class Program
     {
         private static IConfigurationRoot Configuration { get; set; }
+
+        private static JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
 
         public static async Task Main(string[] args)
         {
@@ -18,25 +26,69 @@ namespace Emsi.Playground
 
             Configuration = builder.Build();
 
-            await GetDataAsync();
+            await GetStatusAsync();
+            await GetMetaAsync();
         }
 
-        private static async Task GetDataAsync()
+        private static async Task GetStatusAsync()
         {
-            var skillEndpoint = "https://emsiservices.com/skills";
-
             using var client = new HttpClient();
 
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GetToken());
 
-            var response = await client.GetStringAsync($"{skillEndpoint}/status");
+            StatusDto? dto;
 
-            Console.WriteLine(response);
+            try
+            {
+                dto = await client.GetFromJsonAsync<StatusDto>($"{GetBaseUrl()}/skills/status");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"My message: {e.Message}");
+                throw;
+            }
+
+            if (dto is not null)
+            {
+                Console.WriteLine(dto.Data.Healthy);
+                Console.WriteLine(dto.Data.Message);
+            }
+        }
+
+        private static async Task GetMetaAsync()
+        {
+            using var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GetToken());
+
+            MetaDto? dto;
+
+            try
+            {
+                dto = await client.GetFromJsonAsync<MetaDto>($"{GetBaseUrl()}/skills/meta");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"My message: {e.Message}");
+                throw;
+            }
+
+            if (dto is not null)
+            {
+                Console.WriteLine(dto.Data.Attribution.Title);
+                Console.WriteLine(dto.Data.Attribution.Body);
+                Console.WriteLine(dto.Data.LatestVersion);
+            }
         }
 
         private static string GetToken()
         {
             return Configuration["EmsiSettings:AccessToken"];
+        }
+
+        private static string GetBaseUrl()
+        {
+            return Configuration["EmsiSettings:Url"];
         }
     }
 }
