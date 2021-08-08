ad219bc5196968718c7b3bf47df7b323253a9ea7
MoganRaj12 <moganrajarumugam12@gmail.com>
2021-08-08 19:01:50 +0530

    Added the GetAllVersion method

--- Emsi.Playground/Program.cs
+++ Emsi.Playground/Program.cs
@@ -28,6 +28,7 @@ namespace Emsi.Playground
 
             await GetStatusAsync();
             await GetMetaAsync();
+            await GetAllVersionsAsync();
         }
 
         private static async Task GetStatusAsync()
@@ -79,6 +80,42 @@ namespace Emsi.Playground
                 Console.WriteLine(dto.Data.Attribution.Body);
                 Console.WriteLine(dto.Data.LatestVersion);
             }
+        }
+
+        //GetVersionsAsync() GET
+        //GetVersionAsync(string version) GET
+        //GetChangesAsync(string version) GET
+        //GetRelatedAsync(List ids) POST
+        //this one has rate limit in place
+        //GetSkillsAndTraceFromDocumentAsync(string version) from../extract/trace endpoint  POST
+
+        private static async Task GetAllVersionsAsync()
+        {
+            using var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GetToken());
+
+            DataDto? dto;
+
+            try
+            {
+                dto = await client.GetFromJsonAsync<DataDto>($"{GetBaseUrl()}/skills/versions");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"My message: {e.Message}");
+                throw;
+            }
+
+            if (dto is not null)
+            {
+                Console.WriteLine("\nList of all versions available:");
+                foreach (var data in dto.Data)
+                    Console.WriteLine(data);
+            }
+
+
         }
 
         private static string GetToken()
