307676c25f69108d2fc357ae2e3f5eafa0f05af8
Vaclav Elias <velias@burning-glass.com>
2021-08-09 18:00:44 +0100

    EmsiClient, EmsiSettings and SkillEndpoint added

---
+++ Emsi.Playground/SkillEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Emsi.Playground.Dtos;
+using System.Threading.Tasks;
+
+namespace Emsi.Playground
+{
+    public class SkillEndpoint
+    {
+        private const string Endpoint = "/skills";
+        private readonly EmsiClient _client;
+
+        public SkillEndpoint(EmsiClient client) => _client = client;
+
+        public Task<StatusDto?> GetStatusAsync()
+            => _client.GetAsync<StatusDto>($"{Endpoint}/status");
+
+        public Task<MetaDto?> GetMetaAsync()
+            => _client.GetAsync<MetaDto>($"{Endpoint}/meta");
+    }
+}
