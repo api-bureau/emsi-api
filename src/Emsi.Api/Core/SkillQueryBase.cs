d1f1f01cee765b7079b1459885533881bb2421d9
Vaclav Elias <velias@burning-glass.com>
2021-08-17 21:28:23 +0100

    SkillQuery added

---
+++ src/Emsi.Api/Core/SkillQueryBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Emsi.Api.Core
+{
+    public class SkillQueryBase
+    {
+        /// <summary>
+        /// Default "id,name,type,infoUrl"
+        /// </summary>
+        public string Fields { get; set; } = "id,name,type,infoUrl";
+
+        public List<string> TypeIds { get; set; } = new List<string>();
+
+
+        //ToDo Implement query buider
+        public virtual string Create()
+        {
+            return string.Empty;
+        }
+    }
+}
