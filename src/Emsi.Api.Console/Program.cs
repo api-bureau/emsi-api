c5d8cdc887ed035747f9694884cb6c1259625c47
Vaclav Elias <velias@burning-glass.com>
2021-08-17 21:30:51 +0100

    Error handling added

--- src/Emsi.Api.Console/Program.cs
+++ src/Emsi.Api.Console/Program.cs
@@ -1,53 +1,62 @@
+using Emsi.Api.Console;
 using Emsi.Api.Console.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 
-namespace Emsi.Api.Console
+public class Program
 {
-    public class Program
+    private static DataService _dataService = default!;
+
+    public static async Task<int> Main(string[] args)
     {
-        private static DataService _dataService = default!;
+        var services = new ServiceCollection();
 
-        public static async Task<int> Main(string[] args)
+        var startup = new Startup();
+
+        startup.ConfigureServices(services);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _dataService = serviceProvider.GetService<DataService>()
+            ?? throw new ArgumentNullException($"{nameof(DataService)} cannot be null");
+
+        var cmd = GetRootCommand();
+
+        return await cmd.InvokeAsync(args);
+    }
+
+    private static RootCommand GetRootCommand()
+    {
+        var cmd = new RootCommand("Emsi.API console application to fetch Emsi API.");
+
+        //cmd.AddCommand(new Command("test", "- test sync")
+        //{
+        //    Handler = CommandHandler.Create(async () =>
+        //    {
+        //        await _synchroniseService.SynchroniseTestAsync();
+        //    })
+        //});
+
+        cmd.AddCommand(SkillsCommand());
+
+        return cmd;
+    }
+
+    private static Command SkillsCommand()
+    {
+        var cmd = new Command("skills", "- get Skills");
+
+        cmd.AddCommand(new Command("status", "- get API status")
         {
-            var services = new ServiceCollection();
+            Handler = CommandHandler.Create(async () => await _dataService.GetStatusAsync())
+        });
 
-            var startup = new Startup();
+        cmd.AddCommand(new Command("meta", "- get API meta"));
 
-            startup.ConfigureServices(services);
-
-            var serviceProvider = services.BuildServiceProvider();
-
-            _dataService = serviceProvider.GetService<DataService>();
-
-            var cmd = GetRootCommand();
-
-            return await cmd.InvokeAsync(args);
-        }
-
-        private static RootCommand GetRootCommand()
-        {
-            var cmd = new RootCommand("Emsi.API console application to fetch Emsi API.");
-
-            cmd.AddCommand(SkillsCommand());
-
-            return cmd;
-        }
-
-        private static Command SkillsCommand()
-        {
-            var cmd = new Command("skills", "- get Skills");
-
-            cmd.AddCommand(new Command("status", "- get API status")
-            {
-                Handler = CommandHandler.Create(async() => await _dataService.GetStatusAsync())
-            });
-
-            cmd.AddCommand(new Command("meta", "- get API meta"));
-
-            return cmd;
-        }
+        return cmd;
     }
 }
+
--- src/Emsi.Api.Console/Startup.cs
+++ src/Emsi.Api.Console/Startup.cs
@@ -1,4 +1,5 @@
 using Emsi.Api.Console.Services;
+using Emsi.Api.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
