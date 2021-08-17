535bb1896741c795c63fa1502552e9d88abe6987
Vaclav Elias <velias@burning-glass.com>
2021-08-17 17:43:51 +0100

    ResponseDto wrapper added

--- src/Emsi.Api/Endpoints/SkillEndpoint.cs
+++ src/Emsi.Api/Endpoints/SkillEndpoint.cs
@@ -18,29 +18,31 @@ namespace Emsi.Api.Endpoints
         public Task<ResponseDto<MetaDto>> GetMetaAsync()
             => _client.GetAsync<MetaDto>($"{Endpoint}/meta");
 
-        //public Task<VersionsDto?> GetVersionsAsync()
-        //    => _client.GetAsync<VersionsDto>($"{Endpoint}/versions");
+        public Task<ResponseDto<List<string>>> GetVersionsAsync()
+            => _client.GetAsync<List<string>>($"{Endpoint}/versions");
+        public Task<ResponseDto<SkillDto>> GetAsync(string version, string id)
+            => _client.GetAsync<SkillDto>($"{Endpoint}/versions/{version}/skills/{id}");
 
         public Task<ResponseDto<VersionMetadataDto>> GetVersionsMetaDataAsync(string version)
             => _client.GetAsync<VersionMetadataDto>($"{Endpoint}/versions/{version}");
 
-        //public Task<VersionChangesDto?> GetVersionChangesAsync(string version)
-        //    => _client.GetAsync<VersionChangesDto>($"{Endpoint}/versions/{version}/changes");
+        public Task<ResponseDto<VersionChangesDto>> GetVersionChangesAsync(string version)
+            => _client.GetAsync<VersionChangesDto>($"{Endpoint}/versions/{version}/changes");
 
-        //public Task<RelatedSkillsDto?> GetRelatedSkillsAsync(string version, RequestIdsDto ids)
-        //    => _client.PostAsync<RelatedSkillsDto>($"{Endpoint}/versions/{version}/related", ids);
-
-        //public Task<SourceTracingDto?> GetExtractSkillsSourceTracing(string version, RequestSourceTraceDto text)
-        //    => _client.PostAsync<SourceTracingDto>($"{Endpoint}/versions/{version}/extract/trace", text);
+        //public Task<ResponseDto<List<SkillDto>>> GetAsync(string version, string query, string typeIds, string fields, int limit)
+        //    => _client.GetAsync<List<SkillDto>>($"{Endpoint}/versions/{version}/skills?{queryparams}");
 
         public Task<ResponseDto<List<SkillDto>>> GetSkillsAsync(string version, string queryparams)
             => _client.GetAsync<List<SkillDto>>($"{Endpoint}/versions/{version}/skills?{queryparams}");
 
-        public Task<ResponseDto<SkillDto>> GetAsync(string version, string id)
-            => _client.GetAsync<SkillDto>($"{Endpoint}/versions/{version}/skills/{id}");
+        public Task<ResponseDto<List<SkillDto>>> GetRelatedSkillsAsync(string version, RequestIdsDto ids)
+            => _client.PostAsync<List<SkillDto>>($"{Endpoint}/versions/{version}/related", ids);
 
-        //public Task<SkillsDto?> GetSkillAsync(string version, RequestIdsDto ids, string queryparams)
-        //    => _client.PostAsync<SkillsDto>($"{Endpoint}/versions/{version}/skills?{queryparams}", ids);
+        //public Task<SourceTracingDto?> GetExtractSkillsSourceTracing(string version, RequestSourceTraceDto text)
+        //    => _client.PostAsync<SourceTracingDto>($"{Endpoint}/versions/{version}/extract/trace", text);
+
+        public Task<ResponseDto<List<SkillDto>>> GetSkillAsync(string version, RequestIdsDto ids, string queryparams)
+            => _client.PostAsync<List<SkillDto>>($"{Endpoint}/versions/{version}/skills?{queryparams}", ids);
 
         //public Task<SkillsDocumentDto?> GetSkillFromDocumentAsync(string version, RequestDocumentDto data)
         //    => _client.PostAsync<SkillsDocumentDto>($"{Endpoint}/versions/{version}/extract", data);
